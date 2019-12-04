be090a81d5abad05b74ff01a38db7e08be445ab3
bruno.rossini <brunorossini@live.com>
2019-12-04 19:51:32 -0300

    first commit

---
+++ precache-manifest.5426c7a62921ca9448378576fcfd59ff.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72716ad42eb33bc089096ae08e2069a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba47f09734389a779f74",
+    "url": "/static/css/2.f7162ef1.chunk.css"
+  },
+  {
+    "revision": "ba47f09734389a779f74",
+    "url": "/static/js/2.eb8d7940.chunk.js"
+  },
+  {
+    "revision": "b2bc56087d613de13897",
+    "url": "/static/js/main.e380d43f.chunk.js"
+  },
+  {
+    "revision": "6c4dfc850d9107569a6b",
+    "url": "/static/js/runtime~main.e5593a08.js"
+  },
+  {
+    "revision": "453a6419bcaf87cf3a73ac536abb4b7c",
+    "url": "/static/media/GroupglobeOn.453a6419.jpg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "/static/media/Roboto-ThinItalic.35a9c89a.ttf"
+  },
+  {
+    "revision": "03bd9d225257adab953ba2a257cf302e",
+    "url": "/static/media/address.03bd9d22.svg"
+  },
+  {
+    "revision": "57f32e55a70adb0e10f60f22ed1cb7fd",
+    "url": "/static/media/alert.57f32e55.svg"
+  },
+  {
+    "revision": "b50e80cc183b8a2d475b3c228d5387ee",
+    "url": "/static/media/alertIconSemSobra.b50e80cc.svg"
+  },
+  {
+    "revision": "127985fd20c5a28913c33362e1aae102",
+    "url": "/static/media/alerts.127985fd.svg"
+  },
+  {
+    "revision": "2c20213692a1988e6f8faf078ebdf62e",
+    "url": "/static/media/battery.2c202136.svg"
+  },
+  {
+    "revision": "a728ab999cc03353f49152319023ffec",
+    "url": "/static/media/block.a728ab99.svg"
+  },
+  {
+    "revision": "523d41fa8244ae5e5f06a9d7c6b5079a",
+    "url": "/static/media/car.523d41fa.svg"
+  },
+  {
+    "revision": "a060c9f7f633ef819995f80a908f8d5b",
+    "url": "/static/media/car.a060c9f7.svg"
+  },
+  {
+    "revision": "80c17e8f3ad58e2dd7f1712921bb14a7",
+    "url": "/static/media/check.80c17e8f.svg"
+  },
+  {
+    "revision": "4ac2f0332384adb2da6a9d8564a8d32d",
+    "url": "/static/media/command.4ac2f033.svg"
+  },
+  {
+    "revision": "71ac9f7b7489dff058b9206d851e8b56",
+    "url": "/static/media/daily.71ac9f7b.svg"
+  },
+  {
+    "revision": "a5f9a6ae1909fa37a26e5285e442802c",
+    "url": "/static/media/directions.a5f9a6ae.svg"
+  },
+  {
+    "revision": "b6ee62966adb9133ea71f3243d33ae28",
+    "url": "/static/media/driver.b6ee6296.svg"
+  },
+  {
+    "revision": "355a2b3906551e82d795c9654d8b2600",
+    "url": "/static/media/espirito-santo.355a2b39.svg"
+  },
+  {
+    "revision": "1da3cd0286eb44d068d7b461dcad6b39",
+    "url": "/static/media/favicon.1da3cd02.png"
+  },
+  {
+    "revision": "e405ab736ef62aac16cec64f600167b0",
+    "url": "/static/media/gear.e405ab73.svg"
+  },
+  {
+    "revision": "b693a5a1152d0db3564bac6839ff472e",
+    "url": "/static/media/globe.b693a5a1.svg"
+  },
+  {
+    "revision": "c61a308ce1de10ccccf634b6a62badaf",
+    "url": "/static/media/iginition.c61a308c.svg"
+  },
+  {
+    "revision": "e0f05193b942152ea409071d054a2e44",
+    "url": "/static/media/infobox.e0f05193.es6"
+  },
+  {
+    "revision": "ba03622b104f44e0edfc17ff4ef30111",
+    "url": "/static/media/journey.ba03622b.svg"
+  },
+  {
+    "revision": "26017e8b4738b252441161e86c0719f5",
+    "url": "/static/media/lastComunication.26017e8b.svg"
+  },
+  {
+    "revision": "39d6b58064504623e421e715e8c03772",
+    "url": "/static/media/maintenance.39d6b580.svg"
+  },
+  {
+    "revision": "6bde3fbfabcd6068ae980aa6743a90f9",
+    "url": "/static/media/markerAlert.6bde3fbf.svg"
+  },
+  {
+    "revision": "7a752213d5405c8da8cc00f9bc81eb05",
+    "url": "/static/media/markerFinish.7a752213.svg"
+  },
+  {
+    "revision": "494a7dd825988534a633ece323820589",
+    "url": "/static/media/markerRun.494a7dd8.svg"
+  },
+  {
+    "revision": "728068b7abc06ba7a1f04f0aa2ed24a9",
+    "url": "/static/media/markerStop.728068b7.svg"
+  },
+  {
+    "revision": "6b1ff9acff7529bda6c148c33c81b1e3",
+    "url": "/static/media/noSignalIconSemSobra.6b1ff9ac.svg"
+  },
+  {
+    "revision": "8f4c4d6623085c383bd624f39cebb1e5",
+    "url": "/static/media/noun_Parking_2672865.8f4c4d66.svg"
+  },
+  {
+    "revision": "37b4b914f764981abf3de3f6e44df2c5",
+    "url": "/static/media/noun_Speedometer_2171226.37b4b914.svg"
+  },
+  {
+    "revision": "731656c63baeb9a211ddc6547e1a01b4",
+    "url": "/static/media/noun_no signal_2452207.731656c6.svg"
+  },
+  {
+    "revision": "b4ba43d7f3d97dda71851813a65996b9",
+    "url": "/static/media/odometer.b4ba43d7.svg"
+  },
+  {
+    "revision": "cee2a418549f45b5b295f0a3f21b37eb",
+    "url": "/static/media/plate.cee2a418.svg"
+  },
+  {
+    "revision": "3113a5ad396d61dc32557f0a848d2384",
+    "url": "/static/media/position.3113a5ad.svg"
+  },
+  {
+    "revision": "df83ec25896cd2a74e608541738e4f13",
+    "url": "/static/media/referencePointOff.df83ec25.svg"
+  },
+  {
+    "revision": "3c9d16395a339a22dc0b3e9ecb10cab6",
+    "url": "/static/media/referencePointOn.3c9d1639.svg"
+  },
+  {
+    "revision": "19b83fc19630f16da751d033d6d97724",
+    "url": "/static/media/report.19b83fc1.svg"
+  },
+  {
+    "revision": "ca30f89e75c6f50ab9a0f07269b439b4",
+    "url": "/static/media/report_image_n1.ca30f89e.svg"
+  },
+  {
+    "revision": "6bde679e185c08cd1e89d333cf1b99f5",
+    "url": "/static/media/route.6bde679e.svg"
+  },
+  {
+    "revision": "6c11dabcfcccdeec246f3d54a9d08d56",
+    "url": "/static/media/rulerOff.6c11dabc.svg"
+  },
+  {
+    "revision": "108f00d3c97290f575bcd35f1be3f3ea",
+    "url": "/static/media/rulerOn.108f00d3.svg"
+  },
+  {
+    "revision": "a30821833967b49a47c1938549569ec3",
+    "url": "/static/media/runIconSemSobra.a3082183.svg"
+  },
+  {
+    "revision": "6d8a38c2d75939b1e1e316c2f64d8258",
+    "url": "/static/media/search.6d8a38c2.svg"
+  },
+  {
+    "revision": "c979403c5d83a5ff3e6ef5dc5bd18cc9",
+    "url": "/static/media/sleepIconSemSobra.c979403c.svg"
+  },
+  {
+    "revision": "73763393599dcae77530335fdf5fc391",
+    "url": "/static/media/stopIconSemSobra.73763393.svg"
+  },
+  {
+    "revision": "ad7758c744ea80a0a61b52b6f511c3bb",
+    "url": "/static/media/supply.ad7758c7.svg"
+  },
+  {
+    "revision": "c331336fd7010990aef9c6a0d1f09dba",
+    "url": "/static/media/surveillance.c331336f.svg"
+  },
+  {
+    "revision": "52bb521fc3b1b86293b4ccd124d56592",
+    "url": "/static/media/totalIconSemSobra.52bb521f.svg"
+  },
+  {
+    "revision": "4ff95aa7fcb77aae46ba723abdad5cfe",
+    "url": "/static/media/trafficOff.4ff95aa7.png"
+  },
+  {
+    "revision": "2b0d1717c6d7176883559a59b696d305",
+    "url": "/static/media/trafficOn.2b0d1717.png"
+  },
+  {
+    "revision": "3e4dc1cdabded7f1826a52b1d59a8930",
+    "url": "/static/media/transmitter.3e4dc1cd.svg"
+  },
+  {
+    "revision": "0f98f1a7b48d234b69baa0d2898cb538",
+    "url": "/static/media/violation.0f98f1a7.svg"
+  },
+  {
+    "revision": "f72fef122a8d2368ae90eaa3ddb359cf",
+    "url": "/static/media/widgetMinimap.f72fef12.svg"
+  },
+  {
+    "revision": "635098c7416f47929c46694ebd1a6429",
+    "url": "/static/media/widgetStreetMap.635098c7.svg"
+  },
+  {
+    "revision": "2be317de659a9f1cbeb6ac60bf60ff00",
+    "url": "/static/media/zone.2be317de.svg"
+  },
+  {
+    "revision": "d164690732ecc3182ef6ae835ba7bf50",
+    "url": "/static/media/zoneOff.d1646907.svg"
+  },
+  {
+    "revision": "6a7e99742888a7c84c1c56d6bb0240fa",
+    "url": "/static/media/zoneOn.6a7e9974.svg"
+  }
+]);
